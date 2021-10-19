bf7943fc19c59545fdd7bf51dfc1d117ee7a3c17
marcintyminski <81477027+marcintyminski@users.noreply.github.com>
2021-10-19 19:23:40 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,93 @@
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    sprite += -1
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    sprite += 1
+})
+let mySprite: Sprite = null
+scene.setBackgroundColor(9)
+let sprite = 0
+let mySprite2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . b 5 5 b . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . b b b b b 5 5 5 5 5 5 5 b . . 
+    . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+    . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+    . . b d 5 5 b 1 f f 5 4 4 c . . 
+    b b d b 5 5 5 d f b 4 4 4 4 b . 
+    b d d c d 5 5 b 5 4 4 4 4 4 4 b 
+    c d d d c c b 5 5 5 5 5 5 5 b . 
+    c b d d d d d 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Player)
+let mySprite3 = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+let mySprite4 = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ........................
+    ..........ffff..........
+    ........ff1111ff........
+    .......fb111111bf.......
+    .......f11111111f.......
+    ......fd11111111df......
+    ......fd11111111df......
+    ......fddd1111dddf......
+    ......fbdbfddfbdbf......
+    ......fcdcf11fcdcf......
+    .......fb111111bf.......
+    ......fffcdb1bdffff.....
+    ....fc111cbfbfc111cf....
+    ....f1b1b1ffff1b1b1f....
+    ....fbfbffffffbfbfbf....
+    .........ffffff.........
+    ...........fff..........
+    ........................
+    ........................
+    ........................
+    ........................
+    `, SpriteKind.Player)
+mySprite2.setPosition(80, 60)
+mySprite3.setPosition(40, 60)
+mySprite4.setPosition(120, 60)
+forever(function () {
+    if (sprite == 0) {
+        mySprite = mySprite2
+        mySprite2.setPosition(80, 60)
+        mySprite3.setPosition(40, 60)
+        mySprite4.setPosition(120, 60)
+    } else if (sprite == 1) {
+        mySprite2.setPosition(120, 60)
+        mySprite3.setPosition(80, 60)
+        mySprite4.setPosition(40, 60)
+        mySprite = mySprite3
+    } else if (sprite == 2) {
+        mySprite2.setPosition(40, 60)
+        mySprite3.setPosition(120, 60)
+        mySprite4.setPosition(80, 60)
+        mySprite = mySprite4
+    }
+    mySprite.setPosition(80, 100)
+})

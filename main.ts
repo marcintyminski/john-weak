315e5fc508c205e4573c47875f72870a587bda67
EarthOddity <91958664+EarthOddity@users.noreply.github.com>
2021-10-21 20:01:42 +0300

    put up enemies

--- main.ts
+++ main.ts
@@ -13,72 +13,30 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
 let sprite = 0
 let mySprite: Sprite = null
 scene.setBackgroundColor(7)
-let mySprite2 = sprites.create(assets.image`John Weak`, SpriteKind.Player)
-let mySprite3 = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Player)
-let mySprite4 = sprites.create(img`
-    ........................
-    ........................
-    ........................
-    ........................
-    ..........ffff..........
-    ........ff1111ff........
-    .......fb111111bf.......
-    .......f11111111f.......
-    ......fd11111111df......
-    ......fd11111111df......
-    ......fddd1111dddf......
-    ......fbdbfddfbdbf......
-    ......fcdcf11fcdcf......
-    .......fb111111bf.......
-    ......fffcdb1bdffff.....
-    ....fc111cbfbfc111cf....
-    ....f1b1b1ffff1b1b1f....
-    ....fbfbffffffbfbfbf....
-    .........ffffff.........
-    ...........fff..........
-    ........................
-    ........................
-    ........................
-    ........................
-    `, SpriteKind.Player)
+let mainCharacter = sprites.create(assets.image`John Weak`, SpriteKind.Player)
+let monster = sprites.create(assets.image`Monster`, SpriteKind.Enemy)
+let criminal = sprites.create(assets.image`Criminal`, SpriteKind.Enemy)
 let ball = sprites.create(assets.image`ball`, SpriteKind.Collectible)
-mySprite2.setPosition(80, 60)
-mySprite2.setPosition(80, 60)
-mySprite3.setPosition(40, 60)
-mySprite4.setPosition(120, 60)
+mainCharacter.setPosition(80, 60)
+mainCharacter.setPosition(80, 60)
+monster.setPosition(40, 60)
+criminal.setPosition(120, 60)
 forever(function () {
     if (sprite == 0) {
-        mySprite = mySprite2
+        mySprite = mainCharacter
     } else if (sprite == 1) {
-        mySprite2.setPosition(80, 60)
-        mySprite3.setPosition(40, 60)
-        mySprite4.setPosition(120, 60)
-        mySprite2.setPosition(120, 60)
-        mySprite3.setPosition(80, 60)
-        mySprite4.setPosition(40, 60)
-        mySprite = mySprite3
+        mainCharacter.setPosition(80, 60)
+        monster.setPosition(40, 60)
+        criminal.setPosition(120, 60)
+        mainCharacter.setPosition(120, 60)
+        monster.setPosition(80, 60)
+        criminal.setPosition(40, 60)
+        mySprite = monster
     } else if (sprite == 2) {
-        mySprite2.setPosition(40, 60)
-        mySprite3.setPosition(120, 60)
-        mySprite4.setPosition(80, 60)
-        mySprite = mySprite4
+        mainCharacter.setPosition(40, 60)
+        monster.setPosition(120, 60)
+        criminal.setPosition(80, 60)
+        mySprite = criminal
     }
     mySprite.setPosition(80, 100)
 })

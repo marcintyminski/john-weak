234c42de961dc53724488af12645e420759500dc
marcintyminski <81477027+marcintyminski@users.noreply.github.com>
2021-10-22 00:20:22 +0200

    physics

--- main.ts
+++ main.ts
@@ -1,34 +1,34 @@
 namespace SpriteKind {
     export const Collectible = SpriteKind.create()
     export const theGoal = SpriteKind.create()
+    export const coin = SpriteKind.create()
+    export const flower = SpriteKind.create()
+    export const x = SpriteKind.create()
 }
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite.startEffect(effects.halo)
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    game.over(true)
 })
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    sprite += -1
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    mainCharacter.vy = -180
 })
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    sprite += 1
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
+    game.over(false)
 })
-let sprite = 0
-let mySprite: Sprite = null
-scene.setBackgroundColor(7)
-let mainCharacter = sprites.create(assets.image`John Weak`, SpriteKind.Player)
-let monster = sprites.create(assets.image`Monster`, SpriteKind.Enemy)
-let criminal = sprites.create(assets.image`Criminal`, SpriteKind.Enemy)
-let aliens = sprites.create(assets.image`Alien`, SpriteKind.Enemy)
-let neighbour = sprites.create(assets.image`Neighbour`, SpriteKind.Enemy)
-let ball = sprites.create(assets.image`ball`, SpriteKind.Collectible)
-let collar = sprites.create(assets.image`collar`, SpriteKind.Collectible)
-let bone = sprites.create(assets.image`bone`, SpriteKind.Collectible)
-let leash = sprites.create(assets.image`leash`, SpriteKind.Collectible)
-let TheDog = sprites.create(assets.image`Dog2`, SpriteKind.theGoal)
-mainCharacter.setPosition(80, 60)
-mainCharacter.setPosition(80, 60)
-monster.setPosition(40, 60)
-criminal.setPosition(120, 60)
-forever(function () {
+function choose_skin () {
+    let sprite = 0
+    monster = sprites.create(assets.image`Monster`, SpriteKind.Enemy)
+    criminal = sprites.create(assets.image`Criminal`, SpriteKind.Enemy)
+    aliens = sprites.create(assets.image`Alien`, SpriteKind.Enemy)
+    neighbour = sprites.create(assets.image`Neighbour`, SpriteKind.Enemy)
+    ball = sprites.create(assets.image`ball`, SpriteKind.Collectible)
+    collar = sprites.create(assets.image`collar`, SpriteKind.Collectible)
+    bone = sprites.create(assets.image`bone`, SpriteKind.Collectible)
+    leash = sprites.create(assets.image`leash`, SpriteKind.Collectible)
+    TheDog = sprites.create(assets.image`Dog2`, SpriteKind.theGoal)
+    mainCharacter.setPosition(80, 60)
+    mainCharacter.setPosition(80, 60)
+    monster.setPosition(40, 60)
+    criminal.setPosition(120, 60)
     if (sprite == 0) {
         mySprite = mainCharacter
     } else if (sprite == 1) {
@@ -46,4 +46,87 @@ forever(function () {
         mySprite = criminal
     }
     mySprite.setPosition(80, 100)
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.coin, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.x, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    enemy2 = sprites.create(assets.image`Monster`, SpriteKind.Enemy)
+    enemy2.setPosition(158, 59)
+})
+let mySprite: Sprite = null
+let TheDog: Sprite = null
+let leash: Sprite = null
+let bone: Sprite = null
+let collar: Sprite = null
+let ball: Sprite = null
+let neighbour: Sprite = null
+let aliens: Sprite = null
+let criminal: Sprite = null
+let monster: Sprite = null
+let coins: Sprite = null
+let x: Sprite = null
+let mainCharacter: Sprite = null
+let enemy2: Sprite = null
+scene.setBackgroundColor(9)
+tiles.setTilemap(tilemap`level1`)
+info.setScore(0)
+info.setLife(3)
+enemy2.follow(mainCharacter)
+mainCharacter = sprites.create(assets.image`John Weak`, SpriteKind.Player)
+controller.moveSprite(mainCharacter, 100, 0)
+scene.cameraFollowSprite(mainCharacter)
+mainCharacter.ay = 300
+tiles.placeOnRandomTile(mainCharacter, assets.tile`myTile`)
+for (let value of tiles.getTilesByType(assets.tile`myTile`)) {
+    tiles.setTileAt(value, assets.tile`transparency16`)
+}
+for (let value of tiles.getTilesByType(assets.tile`myTile2`)) {
+    x = sprites.create(img`
+        f f f . . . . . . . . . . f f f 
+        f f f f . . . . . . . . f f f f 
+        f f f f f . . . . . . f f f f f 
+        . f f f f f . . . . f f f f f . 
+        . . f f f f f . . f f f f f . . 
+        . . . f f f f f f f f f f . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . f f f f f f f f f f . . . 
+        . . f f f f f . . f f f f f . . 
+        . f f f f f . . . . f f f f f . 
+        f f f f f . . . . . . f f f f f 
+        f f f f . . . . . . . . f f f f 
+        f f f . . . . . . . . . . f f f 
+        `, SpriteKind.x)
+    tiles.placeOnTile(x, value)
+    tiles.setTileAt(value, assets.tile`transparency16`)
+}
+for (let value of tiles.getTilesByType(assets.tile`myTile0`)) {
+    coins = sprites.create(img`
+        . . . . . f f f f f f f . . . . 
+        . . . f f 5 5 5 5 5 5 5 f f . . 
+        . . f 5 5 5 5 5 5 5 5 5 5 5 f . 
+        . . f 5 5 5 5 5 5 5 5 5 5 5 f . 
+        . f 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+        . f 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+        . f 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+        . f 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+        . f 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+        . f 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+        . f 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+        . . f 5 5 5 5 5 5 5 5 5 5 5 f . 
+        . . f 5 5 5 5 5 5 5 5 5 5 5 f . 
+        . . . f f 5 5 5 5 5 5 5 f f . . 
+        . . . . . f f f f f f f . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.coin)
+    tiles.placeOnTile(coins, value)
+    tiles.setTileAt(value, assets.tile`transparency16`)
+}
+forever(function () {
+	
 })
